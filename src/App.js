87027a9b88239d27e1d3152709dd14dcf19588ed
olaide-hok <olaidekareem43@yahoo.com>
2022-08-29 08:54:42 +0100

    Setup Code Structure

--- src/App.js
+++ src/App.js
@@ -4,7 +4,32 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <section className='container'>
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={color} 
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#336B87'
+          />
+          <button className="btn" type='submit'>submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>list goes here</h4>
+      </section>
+    </>
+  )
+
 }
 
 export default App
